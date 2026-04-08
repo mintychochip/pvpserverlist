e677f2954130a453abded4b43229e4c86c94d262
mintychochip <jlo2@csub.edu>
2026-04-08 15:49:22 -0700

    fix: switch to static output, use Cloudflare Functions for API

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare({
-    imageService: 'passthrough'
-  }),
+  output: 'static',
   // Deploying to root domain guildpost.tech
   build: {
     assets: 'assets',
